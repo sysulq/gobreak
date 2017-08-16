84754d34a09bcadc95d689470dd5ea7de77cb78b
hnlq715 <hnlq.sysu@gmail.com>
2017-08-16 12:38:46 +0800

    Add recovery and improve errType

--- gobreak.go
+++ gobreak.go
@@ -6,42 +6,59 @@ type runFunc func() error
 type fallbackFunc func(error) error
 
 const (
-	Success   = "success"
-	ErrReject = "reject"
-	ErrFail   = "fail"
+	Success     = "success"
+	ErrReject   = "reject"
+	ErrFallBack = "fallback"
+	ErrFail     = "fail"
+	ErrPanic    = "panic"
 )
 
 // Do runs your function in a synchronous manner, blocking until either your function succeeds
 // or an error is returned, including circuit errors
 func Do(name string, run runFunc, fall fallbackFunc) error {
+	errorType := Success
+	// obtain circuit by name
 	c := getCircuit(name)
 
+	// ask circuit allow run or not
 	done, err := c.Allow()
 	if err != nil {
-		requests.WithLabelValues(name, ErrReject).Inc()
+		errorType = ErrReject
 		if fall != nil {
+			errorType = ErrFallBack
 			err = fall(err)
 		}
+		requests.WithLabelValues(name, errorType).Inc()
 		return err
 	}
 
 	now := time.Now()
 
+	// process run function
 	err = run()
+	// try recover when run function panics
+	defer func() {
+		e := recover()
+		if e != nil {
+			done(false)
+			requests.WithLabelValues(name, ErrPanic).Inc()
+			panic(e)
+		}
+	}()
 
 	elapsed := time.Now().Sub(now).Seconds()
 	requestLatencyHistogram.WithLabelValues(name).Observe(elapsed)
 
+	// report run results to circuit
+	done(err == nil)
 	if err != nil {
-		done(false)
-		requests.WithLabelValues(name, ErrFail).Inc()
+		errorType = ErrFail
 		if fall != nil {
+			errorType = ErrFallBack
 			err = fall(err)
 		}
-		return err
 	}
 
-	done(true)
-	requests.WithLabelValues(name, Success).Inc()
-	return nil
+	requests.WithLabelValues(name, errorType).Inc()
+	return err
 }
